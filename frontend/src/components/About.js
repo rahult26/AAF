dfe3dcae82ef33648272afd41c61c97cd1b38c02
rahult26 <b8019589@my.shu.ac.uk>
2022-03-14 14:28:48 +0000

    about us updated

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className="col-md-6">
           <h1 className="display-6 mb-2">Who <b>We</b> Are</h1>
           <hr className="w-50"/>
-          <p className="lead mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi odit dolorum, necessitatibus dignissimos reprehenderit dicta id rem. Exercitationem soluta ipsam necessitatibus aliquid totam recusandae a alias optio eum quas, corrupti ducimus cupiditate aut adipisci eos tempora quam similique nemo quo, vel dicta blanditiis. Esse magnam laudantium officiis et sed quis?</p>
+          <p className="lead mb-4">RBO is a book request application where users can request any books they desire and we will aim to provide the books.</p>
           <a className="btn btn-primary rounded-pill px-4 py-2" href="/books">Get Started</a>
         </div>
       </div>
